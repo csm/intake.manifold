2444305130f610ddb0dd0ac1acc83403fac128ae
Casey Marshall <csm@noonhome.com>
2019-08-28 12:09:34 -0700

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/intake "0.1.2"
+(defproject com.github.csm/intake "0.1.3-SNAPSHOT"
   :description "Bindings-preserving manifold.deferred operations, core.async compat for manifold"
   :url "https://github.com/csm/intake.manifold"
   :license {:name "MIT"
