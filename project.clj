b1ea648258759b17fdaae659fb98b24438404cd0
Casey Marshall <casey@metastatic.org>
2020-06-18 14:13:13 -0700

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/intake "0.1.4"
+(defproject com.github.csm/intake "0.1.5-SNAPSHOT"
   :description "Bindings-preserving manifold.deferred operations, core.async compat for manifold"
   :url "https://github.com/csm/intake.manifold"
   :license {:name "MIT"
